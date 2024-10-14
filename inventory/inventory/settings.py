fb122f6bad780f36ea81bd5fc673f310df906a94
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-14 15:42:44 +0000

    updated views.py

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory_app' # My app
+    'inventory_app', # My app
 ]
 
 MIDDLEWARE = [
