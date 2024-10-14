2d1d8efbfe0bf492d3d7b7c8b37f8a2189135639
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-14 17:52:30 +0000

    made changes to item class

--- inventory/inventory_app/models.py
+++ inventory/inventory_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):  
     # Define your fields here
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    stock = models.IntegerField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
