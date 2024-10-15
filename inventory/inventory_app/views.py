28255fd45c88ebda54bddd2f3ac3d44dd8e723c7
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-15 15:31:28 +0000

    added view to list all items to views.py

--- inventory/inventory_app/views.py
+++ inventory/inventory_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
-
+from .models import Item
+from django.http import HttpResponse
 
 # Create your views here.
 
-
+# View to list all items
+def item_list(request):
+    items = Item.objects.all()  # Fetch all items from the database
+    return render(request, 'inventory/item_list.html', {'items': items})
